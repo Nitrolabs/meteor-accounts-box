2422304f7e428cbcf15494ca152d866eeb0321a7
Giovanni Campagna <scampa.giovanni@gmail.com>
2014-10-23 19:42:20 -0700

    More skeleton code
    Now only the actual OAuth step is necessary

--- box.js
+++ box.js
@@ -1,1 +1,23 @@
-// Write your package code here!
+Accounts.oauth.registerService('box');
+
+if (Meteor.isClient) {
+    Meteor.loginWithBox = function(options, callback) {
+        // support a callback without options
+        if (!callback && typeof options === "function") {
+            callback = options;
+            options = {};
+        }
+        if (!options)
+            options = {};
+
+
+        var credentialRequestCompleteCallback = Accounts.oauth.credentialRequestCompleteHandler(callback);
+        Box.requestCredential(options, credentialRequestCompleteCallback);
+    };
+} else {
+    // XXX: decide what to put here
+    Accounts.addAutopublishFields({
+        forLoggedInUser: [],
+        forOtherUsers: []
+    });
+}
--- package.js
+++ package.js
@@ -7,11 +7,17 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('undefined');
+  api.use('accounts-base', ['client', 'server']);
+  // Export Accounts (etc) to packages using this one.
+  api.imply('accounts-base', ['client', 'server']);
+  api.use('accounts-oauth', ['client', 'server']);
+
   api.addFiles('box.js');
 });
 
 Package.onTest(function(api) {
   api.use('tinytest');
   api.use('gcampax:accounts-box');
+
   api.addFiles('box-tests.js');
 });
